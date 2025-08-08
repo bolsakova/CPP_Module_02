87d1b31fbd45d89eb558ec92623ed7c887442909
bolsakova <tanjutik162@gmail.com>
2025-08-08 19:46:59 +0200

    ex02: added prototypes of functions to overload operators and learned the basics about them

--- ex02/Fixed.hpp
+++ ex02/Fixed.hpp
@@ -8,11 +8,11 @@
 class Fixed {
 
 	private:
-			int _fixedPointValue;					// integer to keep
-			static const int _fractionalBits = 8;	// always 8 bits for fractional part
+			int					_fixedPointValue;					// integer to keep
+			static const int	_fractionalBits = 8;	// always 8 bits for fractional part
 
 	public:
-			
+			// Constructors and destructor
 			Fixed();								// Default constructor - initialized value with 0
 			Fixed(int const _intValue);				// Constructor with parameter (int) - converts a constant integer to the corresponding fixed-point value
 			Fixed(float const _floatValue);			// Constructor with parameter (float) - converts a constant floating-point number to the corresponding fixed-point value
@@ -20,11 +20,36 @@ class Fixed {
 			Fixed&	operator=(const Fixed& other);	// Assignment operator - assigning (1) copy the data from one already existing object to another one (2) is called when c = b; (с is already created)
 			~Fixed();								// Destructor - destroys the object, frees resources (memory, sockets, files etc.)
 			
+			// Methods
 			float	toFloat( void ) const;			// member function converts the fixed-point value to a floating-point value
 			int		toInt( void ) const;			// member function converts the fixed-point value to an int value
+
+			// Overload of comparison operators
+			bool	operator>(const Fixed& other) const;
+			bool	operator<(const Fixed& other) const;
+			bool	operator>=(const Fixed& other) const;
+			bool	operator<=(const Fixed& other) const;
+			bool	operator==(const Fixed& other) const;
+			bool	operator!=(const Fixed& other) const;
+
+			// Overload of arithmetic operators
+			Fixed	operator+(const Fixed& other) const;
+			Fixed	operator-(const Fixed& other) const;
+			Fixed	operator*(const Fixed& other) const;
+			Fixed	operator/(const Fixed& other) const;
+
+			// Overload of increment/decrement operators
+			// prefix
+			Fixed&	operator++();
+			Fixed&	operator--();
+			// postfix
+			Fixed	operator++(int);
+			Fixed	operator--(int);
+
+			// 
+
 };
-// an overload of the insertion (<<)
-//		- inserts a floating-point representation of the fixed-point number into the output stream object passed as a parameter
+// Overload of the insertion (<<)
 std::ostream& operator<<(std::ostream& out, const Fixed& fixed);
 
 #endif
